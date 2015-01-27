b12244e3d97d8ae0d8706f5378ed353c7b35fd41
Greg Maka <makagreg@yahoo.com>
2015-01-26 19:42:50 -0600

    Added function display_array_neat($arr) to display sorted multi-dimensional arrays more attractively.

--- ch01/sort.php
+++ ch01/sort.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Sorting Multidimensional Arrays</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <title>Sorting Multidimensional Arrays</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php # Script 1.1 - sort.php
@@ -27,7 +27,7 @@ $students = array(
 
 // Name sorting function:
 function name_sort($x, $y) {
-	return strcasecmp($x['name'], $y['name']);
+  return strcasecmp($x['name'], $y['name']);
 }
 
 // Grade sorting function:
@@ -37,16 +37,48 @@ function grade_sort($x, $y) {
 }
 
 // Print the array as is:
-echo '<h2>Array As Is</h2><pre>' . print_r($students, 1) . '</pre>';
+//echo '<h2>Array As Is</h2><pre>' . print_r($students, 1) . '</pre>';
 
 // Sort by name:
-uasort($students, 'name_sort');
-echo '<h2>Array Sorted By Name</h2><pre>' . print_r($students, 1) . '</pre>';
+//uasort($students, 'name_sort');
+//echo '<h2>Array Sorted By Name</h2><pre>' . print_r($students, 1) . '</pre>';
 
 // Sort by grade:
+//uasort($students, 'grade_sort');
+//echo '<h2>aArray Sorted By Grade</h2><pre>' . print_r($students, 1) . '</pre>';
+
+/******************Chapter 1 Enhancements***********************/
+// Greg Maka: Added a function for a more attractive output.
+
+/* Function takes a multidimensional array as parameter and
+   displays the values neatly in with html definition tags*/
+
+function display_array_neat($students){
+  echo "<dl";
+  foreach($students as $index => $inner_arr){
+    echo "<dt>"."Student ID = "."$index"."</dt>";
+      foreach($inner_arr as $key => $value){
+        echo "<dd>".ucfirst($key)." = ".$value."</dd>";
+      }
+  }
+  echo "</dl>";
+}
+
+//display array as is
+echo '<h2>Array As Is</h2>';
+display_array_neat($students);
+
+//display array sorted by name
+echo '<h2>Array Sorted by Name</h2>';
+uasort($students, 'name_sort');
+display_array_neat($students);
+
+//display array sorted by grade
 uasort($students, 'grade_sort');
-echo '<h2>Array Sorted By Grade</h2><pre>' . print_r($students, 1) . '</pre>';
+echo '<h2>Array Sorted By Grade</h2>';
+display_array_neat($students);
 
 ?>
+
 </body>
 </html>
